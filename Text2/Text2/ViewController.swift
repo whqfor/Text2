6d470bbe325e2f082f0f5598df11651e319c892e
whqfor <whqfor@wanghuaqiangdeMacBook-Pro-2.local>
2016-05-30 19:03:42 +0800

    add text2 swift

---
+++ Text2/Text2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Text2
+//
+//  Created by 王化强 on 16/5/30.
+//  Copyright © 2016年 whqfor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.cyanColor()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
